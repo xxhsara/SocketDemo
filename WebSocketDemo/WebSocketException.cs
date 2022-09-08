1ad99f14722e8ad4ebb911b9493a3d795f2a6d1e
xxhsara <2103309470@qq.com>
2022-09-08 22:26:55 +0800

    WebSocket 1

---
+++ WebSocketDemo/WebSocketException.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.WebSockets;
+
+namespace WebSocketDemo
+{
+    public static class WebSocketException
+    {
+        public static async Task Echo(this HttpContext context,WebSocket webSocket)
+        {
+            CancellationToken cancellationToken = new CancellationToken();
+            while(webSocket.State==WebSocketState.Open)
+            {
+                ArraySegment<byte> buf = new ArraySegment<byte>(new byte[2048]);
+                //WebSocketReceiveResult result = await webSocket.ReceiveAsync(buf, cancellationToken);
+                await webSocket.SendAsync(buf, WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+        }
+    }
+}
