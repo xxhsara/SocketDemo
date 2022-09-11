ebe503c3134fe2c573e2b7ab717ebd23fa941774
xxhsara <2103309470@qq.com>
2022-09-11 21:37:43 +0800

    WebSocket

---
+++ WebSocketDemo/Controllers/MyWebSocketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSocketDemo.Controllers
+{
+    public class MyWebSocketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- WebSocketDemo/WebSocketException.cs
+++ WebSocketDemo/WebSocketException.cs
@@ -7,10 +7,10 @@ namespace WebSocketDemo
         public static async Task Echo(this HttpContext context,WebSocket webSocket)
         {
             CancellationToken cancellationToken = new CancellationToken();
-            while(webSocket.State==WebSocketState.Open)
+            while (webSocket.State == WebSocketState.Open)
             {
                 ArraySegment<byte> buf = new ArraySegment<byte>(new byte[2048]);
-                //WebSocketReceiveResult result = await webSocket.ReceiveAsync(buf, cancellationToken);
+                WebSocketReceiveResult result = await webSocket.ReceiveAsync(buf, cancellationToken); //这一行不可以注释掉，不然的话前端会一直接收到消息
                 await webSocket.SendAsync(buf, WebSocketMessageType.Text, true, CancellationToken.None);
             }
         }
